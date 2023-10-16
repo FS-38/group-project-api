9f1a631d8adcf891143378f0096530cdd9a5b77f
Muhammad Saman <samanmuhammad077@gmail.com>
2023-10-17 00:18:41 +0800

    feat: add new feature post new story to comunity (10/17/2023 12:19 GMT 8)

--- js/story.js
+++ js/story.js
@@ -1,24 +1,54 @@
-// coding here
 const community = document.getElementById("community-wrapper");
 
-const getStory = async () => {
+const getStoryAndDisplay = async () => {
   const response = await fetch(
     "https://6526adf4917d673fd76cc91f.mockapi.io/api/story"
   );
-  const story = await response.json();
+  const data = await response.json();
 
-  return story;
-};
-getStory().then((data) => {
-  data.map((story) => {
+  // Bersihkan konten HTML pada elemen "community-wrapper"
+  community.innerHTML = "";
+
+  // Tambahkan cerita ke elemen "community-wrapper"
+  data.forEach((story) => {
     const storyCard = `
-    <div class="card" style="width: 60rem" ke>
-              <div class="card-body">
-                <h5 class="card-title">${story.userId}</h5>
-                <p class="card-text">
-                  ${story.content}
-                </p>
-            </div>`;
+      <div class="card" style="width: 60rem">
+        <div class="card-body">
+          <h5 class="card-title">${story.userId}</h5>
+          <p class="card-text">
+            ${story.content}
+          </p>
+        </div>
+      </div>`;
     community.innerHTML += storyCard;
   });
-});
+};
+
+const postStory = async ({ content }) => {
+  await fetch("https://6526adf4917d673fd76cc91f.mockapi.io/api/story", {
+    method: "POST",
+    body: JSON.stringify({
+      userId: "Muhamad Saman",
+      content,
+      like: 0,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
+
+  // Setelah berhasil memposting cerita, panggil fungsi untuk mengambil dan menampilkan cerita terbaru
+  getStoryAndDisplay();
+};
+
+document
+  .getElementsByTagName("form")[0]
+  .addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const inputPost = document.getElementById("input-post").value;
+
+    // Kirim cerita baru
+    await postStory({ content: inputPost });
+    location.reload();
+  });
+getStoryAndDisplay();
