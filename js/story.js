91f40c40ffdfb54e52b22c5eaaa080f5cf1e34fd
Muhammad Saman <samanmuhammad077@gmail.com>
2023-10-16 15:33:14 +0800

    feat: Finishing feature to see story in comunity

--- js/story.js
+++ js/story.js
@@ -1,10 +1,24 @@
 // coding here
+const community = document.getElementById("community-wrapper");
 
 const getStory = async () => {
   const response = await fetch(
     "https://6526adf4917d673fd76cc91f.mockapi.io/api/story"
   );
   const story = await response.json();
-  console.log(story);
+
+  return story;
 };
-getStory();
+getStory().then((data) => {
+  data.map((story) => {
+    const storyCard = `
+    <div class="card" style="width: 60rem" ke>
+              <div class="card-body">
+                <h5 class="card-title">${story.userId}</h5>
+                <p class="card-text">
+                  ${story.content}
+                </p>
+            </div>`;
+    community.innerHTML += storyCard;
+  });
+});
