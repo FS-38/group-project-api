94de57c3ce44564bcee57b2b7aba6a6ac58c41fd
AlgonzaDJun <algonsadewangga@gmail.com>
2023-10-17 19:50:46 +0700

    finish checkout dan sukses

--- js/checkout.js
+++ js/checkout.js
@@ -6,7 +6,7 @@ function confirmation() {
     Swal.fire({
       icon: "error",
       title: "Oops...",
-      text: "Kau harus menyetujui kebijakan privasi!",
+      text: "Kamu harus menyetujui kebijakan privasi!",
     });
     return false;
   } else if (nomorhp.value == "") {
@@ -20,15 +20,45 @@ function confirmation() {
 
   Swal.fire({
     title: "Apakah semua data sudah benar?",
-    text: "Kamu tidak bisa kembali lagi!",
+    text: "Pastikan semua data sudah benar ya",
     icon: "warning",
     showCancelButton: true,
     confirmButtonColor: "#3085d6",
     cancelButtonColor: "#d33",
-    confirmButtonText: "Ya, semua sudah benar!",
+    confirmButtonText: "Ya, sudah benar!",
   }).then((result) => {
     if (result.isConfirmed) {
-      window.location.href('./success.html')
+      window.location.href = "./success.html";
     }
   });
 }
+
+const rupiah = (number) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+  }).format(number);
+};
+
+let dataPayment = localStorage.getItem("payment");
+dataPayment = JSON.parse(dataPayment);
+console.log(dataPayment);
+
+const gambar = document.getElementById("img");
+const namaKonselor = document.getElementById("namadokter");
+const harga = document.getElementById("hargadokter");
+const tggl = document.getElementById("tgl-konseling");
+const hargaAsli = document.getElementById("harga-asli");
+const hargaAfter = document.getElementById("harga-after");
+const hargaAsli2 = document.getElementById("harga-asli2");
+const hargaAfter2 = document.getElementById("harga-after2");
+
+gambar.src = dataPayment.image;
+namaKonselor.innerHTML = dataPayment.name;
+harga.innerHTML = "@" + rupiah(dataPayment.hargaRP) + " / hour";
+tggl.value = dataPayment.tglKonseling;
+hargaAsli.innerHTML = rupiah(dataPayment.hargaRP);
+hargaAfter.innerHTML = rupiah(dataPayment.hargaRP - 5000);
+
+hargaAsli2.innerHTML = rupiah(dataPayment.hargaRP);
+hargaAfter2.innerHTML = rupiah(dataPayment.hargaRP - 5000);
