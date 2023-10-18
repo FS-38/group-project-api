6bd522a8b962a44ea8159a3f521d740f105b7299
Muhammad Saman <samanmuhammad077@gmail.com>
2023-10-18 20:18:52 +0800

    feat: add new feature like story

--- js/story.js
+++ js/story.js
@@ -11,16 +11,48 @@ const getStoryAndDisplay = async () => {
 
   // Tambahkan cerita ke elemen "community-wrapper"
   data.forEach((story) => {
-    const storyCard = `
-      <div class="card" style="width: 60rem">
-        <div class="card-body">
-          <h5 class="card-title">${story.userId}</h5>
-          <p class="card-text">
-            ${story.content}
-          </p>
+    const isLiked = story.userLike.find((data) => data === story.user.userId);
+    const likeButton = document.createElement("button");
+    likeButton.classList.add("btn", "like-button");
+
+    if (isLiked) {
+      likeButton.innerHTML = `
+        <i class="bi bi-heart-fill" style="color:red"></i>
+      `;
+      likeButton.classList.add("liked"); // Tambahkan kelas CSS "liked" untuk mengubah tampilan tombol "Like".
+    } else {
+      likeButton.innerHTML = `
+        <i class="bi bi-heart"></i>
+      `;
+    }
+
+    const storyCard = document.createElement("div");
+    storyCard.classList.add("card");
+    storyCard.style.width = "60rem";
+    storyCard.innerHTML = `
+      <div class="card-body row">
+        <div class="col">
+          <h5 class="card-title">${story.user.name}</h5>
+          <p class="card-text">${story.content}</p>
         </div>
-      </div>`;
-    community.innerHTML += storyCard;
+        <div class="col-1 d-flex flex-column align-items-center">
+          ${likeButton.outerHTML}
+          <span>${story.userLike.length}</span>
+        </div>
+      </div>
+    `;
+
+    const likeElement = storyCard.querySelector(".like-button");
+    likeElement.addEventListener("click", async () => {
+      try {
+        await likeHandler(story, isLiked);
+        getStoryAndDisplay();
+      } catch (error) {
+        console.error(error);
+      }
+    });
+
+    community.appendChild(storyCard);
   });
 };
 
@@ -28,9 +60,11 @@ const postStory = async ({ content }) => {
   await fetch("https://6526adf4917d673fd76cc91f.mockapi.io/api/story", {
     method: "POST",
     body: JSON.stringify({
-      userId: "Muhamad Saman",
+      user: {
+        userId: 1,
+        name: "Muhamad Saman",
+      },
       content,
-      like: 0,
     }),
     headers: {
       "Content-type": "application/json; charset=UTF-8",
@@ -45,10 +79,43 @@ document
   .getElementsByTagName("form")[0]
   .addEventListener("submit", async (event) => {
     event.preventDefault();
-    const inputPost = document.getElementById("input-post").value;
+    const isPost = confirm("Do you really want to post this story?");
+    if (isPost) {
+      const inputPost = document.getElementById("input-post");
+      await postStory({ content: inputPost.value });
+      inputPost.value = "";
+    }
 
     // Kirim cerita baru
-    await postStory({ content: inputPost });
-    location.reload();
   });
+
+const likeHandler = async (story, isLiked) => {
+  let data = [];
+
+  if (isLiked) {
+    data = story.userLike.filter((data) => data !== story.user.userId);
+  } else {
+    data = [...story.userLike, story.user.userId];
+  }
+
+  const response = await fetch(
+    `https://6526adf4917d673fd76cc91f.mockapi.io/api/story/${story.id}`,
+    {
+      method: "PUT",
+      body: JSON.stringify({
+        userLike: data,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }
+  );
+
+  if (response.ok) {
+    return response.json();
+  } else {
+    throw new Error("Gagal mengirim permintaan Like");
+  }
+};
+
 getStoryAndDisplay();
