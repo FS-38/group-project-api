c261a0264ae2d8b25eb0e8f3b131d35cb88793c9
AlgonzaDJun <algonsadewangga@gmail.com>
2023-10-18 23:02:47 +0700

    add css detail konselor, ganti sweet alert, sort cerita

--- js/story.js
+++ js/story.js
@@ -5,6 +5,8 @@ const getStoryAndDisplay = async () => {
     "https://6526adf4917d673fd76cc91f.mockapi.io/api/story"
   );
   const data = await response.json();
+  // sorting data dari terbaru
+  data.sort((a, b) => b.id - a.id);
 
   // Bersihkan konten HTML pada elemen "community-wrapper"
   community.innerHTML = "";
@@ -79,12 +81,25 @@ document
   .getElementsByTagName("form")[0]
   .addEventListener("submit", async (event) => {
     event.preventDefault();
-    const isPost = confirm("Do you really want to post this story?");
-    if (isPost) {
-      const inputPost = document.getElementById("input-post");
-      await postStory({ content: inputPost.value });
-      inputPost.value = "";
-    }
+    // const isPost = confirm("Do you really want to post this story?");
+    // if (isPost) {
+    //   const inputPost = document.getElementById("input-post");
+    //   await postStory({ content: inputPost.value });
+    //   inputPost.value = "";
+    // }
+
+    Swal.fire({
+      title: "Do you really want to post this story?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const inputPost = document.getElementById("input-post");
+        postStory({ content: inputPost.value });
+        inputPost.value = "";  
+      }
+    });
 
     // Kirim cerita baru
   });
